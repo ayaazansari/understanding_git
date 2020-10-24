060dda0f0bb2ff96ea6be27d9e45f59063ec7e1b
Ayaaz <ayaazansari3052@gmail.com>
2020-10-24 23:18:36 +0530

    second ka second branch start

--- app/src/main/java/com/example/gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice/MainActivity.kt
@@ -7,7 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        println("commit after initial commit lol ")
+        println("welcome to the branch of ayaaz")
     }
 }
